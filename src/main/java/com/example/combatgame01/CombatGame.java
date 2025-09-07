59ed6faa2adf9c2480b50474a721ebd0c78121b9
Hendrik_Vilie <chatsbravo@gmail.com>
2025-09-07 20:51:45 +0200

    Added main menu

--- src/main/java/com/example/combatgame01/CombatGame.java
+++ src/main/java/com/example/combatgame01/CombatGame.java
@@ -1,18 +1,47 @@
 package com.example.combatgame01;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 
 public class CombatGame extends GameApplication {
 
     @Override
-    protected void initSettings(GameSettings settings) {
+    protected void initSettings(@NotNull GameSettings settings) {
         settings.setWidth(800);
         settings.setHeight(600);
         settings.setTitle("Combat Game");
         settings.setVersion("1.0");
         settings.setManualResizeEnabled(true);
-    }
+        settings.setFullScreenAllowed(true);
+        settings.setMainMenuEnabled(true);
+        settings.setGameMenuEnabled(true);
+        
+
+
+        }
+
+        public void startGameThread() {
+            // Start game logic here
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+
+        @Override
+        public void run() {
+
+
+            while(gamethread != null) {
+
+            System.out.printl("The loop is running");
+            }
+
+
+            
+
+
+        }
 
     public static void main(String[] args) {
         launch(args); // this comes from FXGL
