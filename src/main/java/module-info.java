7b4eb882454b590b52da478f10e118436d5e3165
Leonel <leonelnelly2@gmail.com>
2025-09-08 14:00:12 +0200

    ghjuiuhkkkhkjhkhk

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.combatgame01 {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires annotations;
 
     opens com.example.combatgame01 to javafx.fxml;
     exports com.example.combatgame01;
